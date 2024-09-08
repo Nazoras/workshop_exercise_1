d368945a57866287175f1e37ac8d054e04003781
Chrissini1 <your-email@example.com>
2024-09-08 10:44:49 +0200

    exercise nummber one: a guessing game

---
+++ guess_the_number_one.py
@@ -0,0 +1,34 @@
+import random
+"""
+a guess game:
+try to figure out, which number we are looking for.
+"""
+
+def my_number():
+    " a helper function to enter the number"
+    try:
+        my_guess = int(input("Guess the number: "))
+    except ValueError:
+        print("It's not a number, last chance fancy pants:")
+        my_guess = int(input("Guess the number: "))
+    return my_guess
+
+def guess_the_number():
+    "the maingame"
+    guess_pc = random.randint(1, 100)
+    given_number = my_number()
+    while given_number > 100 or given_number < 1:
+        print("the number is higher than 100 or negative.")
+        given_number = my_number()
+    while given_number is not guess_pc:
+        if given_number < guess_pc:
+            differenze_small = guess_pc - given_number
+            print(f"Too small!, your number is {differenze_small} higher ")
+            given_number = my_number()
+        else:
+            differenze_high = given_number - guess_pc
+            print(f"Too big! your number is {differenze_high} smaller")
+            given_number = my_number()
+    return print("You Win!")
+
+guess_the_number()
