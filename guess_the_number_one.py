6ed45b34486ab9b8b7d0d12adb2c896d5e464a39
Chrissini1 <your-email@example.com>
2024-09-08 20:33:39 +0200

    normal guess game. everthing works

--- guess_the_number_one.py
+++ guess_the_number_one.py
@@ -11,13 +11,13 @@ def my_number():
             my_guess = int(input("Guess the number: "))
             return my_guess
         except ValueError:
-            print("It's not a number, last chance fancy pants:")
+            print("It's not a number, last chance fancy pants, try again:")
 
 def guess_the_number():
     "the maingame"
     guess_pc = random.randint(1, 100)
     given_number = my_number()
-    while given_number > 100 or given_number < 1:
+    while given_number > 100 or given_number < 0:
         print("the number is higher than 100 or negative.")
         given_number = my_number()
     while given_number is not guess_pc:
